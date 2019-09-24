41f0bc8b6e8e5a06178e79b0c0d86b3f9c9eb525
Charles Kiel <charleskiel@yahoo.com>
2019-09-24 02:28:55 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  
+  a = []
+  
+  counter = 0
+ 
+  while array[counter] do
+  a.insert(-1, array[counter] ** 2)
+    puts array[counter]
+    counter += 1
+  end
+  a
 end
